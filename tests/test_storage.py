6f32ec3abc050d66a4f2b10f894bda80445a65ae
Harry Ha <hatrunghieuhpvn@gmail.com>
2021-06-24 15:26:14 +0700

    Add unit test

---
+++ tests/test_storage.py
@@ -0,0 +1,15 @@
+import pytest
+
+def test_init_number_of_value(storage):
+    number_of_values = storage.getNumberOfValues()
+    assert number_of_values == 0
+
+def test_add_two_values(storage, account):
+    storage.storeValue(1337, {'from': account})
+    storage.storeValue(2008, {'from': account})
+    number_of_values = storage.getNumberOfValues()
+    assert number_of_values == 2
+
+def test_get_first_value(storage):
+    first_value = storage.getValue(0)
+    assert first_value == 1337
