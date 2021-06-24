b961d6541b10e9d12d5a7b849ad420ecaac35fa4
Harry Ha <hatrunghieuhpvn@gmail.com>
2021-06-24 15:36:54 +0700

    Add wait confirmations for each tracnsaction

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -5,8 +5,10 @@ def test_init_number_of_value(storage):
     assert number_of_values == 0
 
 def test_add_two_values(storage, account):
-    storage.storeValue(1337, {'from': account})
-    storage.storeValue(2008, {'from': account})
+    tx = storage.storeValue(1337, {'from': account})
+    tx.wait(3)
+    tx = storage.storeValue(2008, {'from': account})
+    tx.wait(3)
     number_of_values = storage.getNumberOfValues()
     assert number_of_values == 2
 
